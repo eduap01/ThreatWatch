784f07b12870978899d47699f097a5e96db357f0
Eduardo <eduardoarevaloportero@gmail.com>
2025-07-14 19:22:26 +0200

    conexion a base de datos

--- backend/app/core/databse.py
+++ backend/app/core/databse.py
@@ -1,11 +1,18 @@
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-##load_dotenv()
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    print("DATABASE_URL found properly.")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not defined.")
 
 engine = create_engine(DATABASE_URL)
 
